fc7b1c2a95e4a0f6d4181edc6b32f4442b4b5920
krisnawijaksana <83213888+krisnawijaksana@users.noreply.github.com>
2021-04-26 18:40:30 +0800

    Add files via upload

---
+++ app/src/main/java/com/codepath/mypizza/data/Pizza.java
@@ -0,0 +1,24 @@
+package com.codepath.mypizza.data;
+
+/**
+ * Created by Shyam Rokde on 8/5/16.
+ */
+public class Pizza {
+
+  public static String[] pizzaMenu = {
+          "Place 1ST. REQ REGUM QEON",
+          "Place 2ND. BIGETRON",
+          "Place 3RD. ISLAND OF GOD",
+          "Place 4TH. ONIC ESPORT",
+          "Place 5TH. VICTIM"
+  };
+
+  public static String[] pizzaDetails = {
+      "Rex Regum Qeon (RRQ) adalah bahasa latin yang diambil dari kata King of Kings yang artinya adalah raja dari segala raja. Sedangkan untuk kata Qeon merupakan salah satu publisher Indonesia yakni Qeon Interactive.",
+      "Nama Bigetron merupakan gabungan dari dua kata yakni “Bige” dari Bibir Iwet Gaming dan “Ultron” alhasil menjadi nama Bigetron Esports yang kita kenal sekarang berkat performa tim PUBG Mobile mereka yang sangat cemerlang di panggung nasional maupun internasional.",
+      "Sesuai dengan namanya, arti dari tim ini merupakan “pulau para dewa” dan mereka berasal dari pulau Bali atau yang juga memiliki sebutan sebagai pulau Dewata.",
+          "Nama Onic memang tidak mencerminkan kualitas pribadinya, namun memiliki nama yang bagus akan membantu seseorang menjadi lebih percaya diri, dan lebih bersemangat untuk menjadi pribadi yang positif, serta selalu berusaha agar hidupnya dapat bermanfaat untuk banyak orang.",
+        "Nama Victim sendiri merupakan singkatan dari Victory Team eSports yang berdiri sejak 10 September 2018 silam."
+
+  };
+}
